9f9fdf210c357329327ca2b0ade14ea550cb9192
jorainer <johannes.rainer@gmail.com>
2019-05-23 08:30:19 +0200

    Allow specifying column names in FAData constructor

--- R/Constructors.R
+++ R/Constructors.R
@@ -9,29 +9,34 @@
 ##   * sep:
 ##
 ##********************************************************
-FAData <- function(pedigree, age, trait, traitName, header=FALSE, sep="\t",
-                   id.col="id", family.col="family", father.col="father",
-                   mother.col="mother", sex.col="sex"){
+FAData <- function(pedigree, age, trait, traitName, header = FALSE, sep = "\t",
+                   id.col = "id", family.col = "family", father.col = "father",
+                   mother.col = "mother", sex.col = "sex") {
     if(missing(pedigree)){
         stop("Argument pedigree has to be specified!")
     }
     if(missing(trait))
         trait <- logical()
     FSD <- new("FAData")
+    expect_cn <- c(family = family.col, id = id.col, father = father.col,
+                   mother = mother.col, sex = sex.col)
     ## processing pedigree
     if(is.character(pedigree)){
         ## means, we're loading the data.frame ourselfs.
         message(paste("Reading pedigree information from file ", pedigree,
                       "..."), appendLF=FALSE)
-        pedigree <- doImport(pedigree, header=header, sep=sep, id.col=id.col,
-                             family.col=family.col, father.col=father.col,
-                             mother.col=mother.col, sex.col=sex.col)
+        pedigree <- doImport(pedigree, header = header, sep = sep,
+                             id.col = id.col, family.col = family.col,
+                             father.col = father.col, mother.col = mother.col,
+                             sex.col = sex.col)
         ## Check if we've got eventually also the trait (i.e. from a ped or
         ## fam file).
         if(missing(trait) & any(colnames(pedigree) == "trait")){
             trait <- pedigree[, "trait"]
             pedigree <- pedigree[, colnames(pedigree) != "trait"]
         }
+        pedigree <- pedigree[, names(expect_cn)]
+        colnames(pedigree) <- expect_cn
         message("OK\n")
     }
     if (is.matrix(pedigree)) {
@@ -47,12 +52,13 @@ FAData <- function(pedigree, age, trait, traitName, header=FALSE, sep="\t",
         if (is.data.frame(pedigree)) {
             CN <- .PEDCN
             if (!noColNames(pedigree)) {
-                ## check if we have all column names required.
-                if (sum(CN %in% colnames(pedigree)) != length(CN)) {
-                    stop("pedigree is expected to have column names ",
-                         paste(CN, collapse=", "))
-                }
-                pedigree <- pedigree[, CN]
+                if (!all(expect_cn %in% colnames(pedigree)))
+                    stop("Expected column names ",
+                         paste0("'", expect_cn, "'", collapse = ", "),
+                         " in 'pedigree' but I got ",
+                         paste0("'", colnames(pedigree), "'", collapse = ", "))
+                pedigree <- pedigree[, expect_cn]
+                colnames(pedigree) <- names(expect_cn)
             } else {
                 if (ncol(pedigree)!=length(CN))
                     stop("pedigree is expected to have ", length(CN),
@@ -63,7 +69,7 @@ FAData <- function(pedigree, age, trait, traitName, header=FALSE, sep="\t",
             }
         }
     }
-    ## We don't lile factors.
+    ## We don't like factors.
     if (is.factor(pedigree$id)) pedigree$id <- as.character(pedigree$id)
     ## Fixing the founders:
     if (is.factor(pedigree$father))
